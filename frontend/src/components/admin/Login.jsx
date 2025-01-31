7fab2e9542d702ec45f2573d0e931cd3276d70a9
Huzaifa Naseer <huzaifanaseer03@gmail.com>
2025-01-31 05:19:07 +0500

    urls updated 3

--- frontend/src/components/admin/Login.jsx
+++ frontend/src/components/admin/Login.jsx
@@ -65,7 +65,7 @@ export default function Login() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/admin/auth/login', {
+      const response = await fetch('admin/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
